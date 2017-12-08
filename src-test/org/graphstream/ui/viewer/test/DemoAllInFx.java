56d74cf40e708507f4299f05e72761c7ff10ed56
Hicham Brahimi <hicham.brahimi@laposte.net>
2017-12-08 17:16:46 +0100

    Change test package name

--- src-test/org/graphstream/ui/viewer/test/DemoAllInFx.java
+++ src-test/org/graphstream/ui/viewer/test/DemoAllInFx.java
@@ -29,7 +29,7 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C and LGPL licenses and that you accept their terms.
  */
-package org.graphstream.ui.viewer_fx.test;
+package org.graphstream.ui.viewer.test;
 
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.MultiGraph;
