814aebad15ffa8d856f35ca708a7650729fa3074
hichbra <hicham.brahimi@laposte.net>
2018-01-12 18:00:42 +0100

    Improve sync with transforming Viewer Interface to abstract class

--- src/org/graphstream/ui/fx_viewer/FxViewer.java
+++ src/org/graphstream/ui/fx_viewer/FxViewer.java
@@ -31,31 +31,20 @@
  */
 package org.graphstream.ui.fx_viewer;
 
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.logging.Logger;
-
-import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
-import org.graphstream.graph.Node;
 import org.graphstream.stream.ProxyPipe;
 import org.graphstream.stream.Source;
 import org.graphstream.stream.thread.ThreadProxyPipe;
-import org.graphstream.ui.geom.Point3;
 import org.graphstream.ui.graphicGraph.GraphicGraph;
 import org.graphstream.ui.javafx.FxFullGraphRenderer;
-import org.graphstream.ui.layout.Layout;
-import org.graphstream.ui.layout.LayoutRunner;
-import org.graphstream.ui.layout.Layouts;
-import org.graphstream.ui.view.Camera;
 import org.graphstream.ui.view.GraphRenderer;
 import org.graphstream.ui.view.View;
 import org.graphstream.ui.view.Viewer;
-import org.graphstream.ui.view.ViewerPipe;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
+
 /**
  * Set of views on a graphic graph.
  * 
@@ -102,56 +91,10 @@ import javafx.util.Duration;
  * the events that occured on the source before it is connected to it.
  * </p>
  */
-public class FxViewer implements Viewer {
-
-	/**
-	 * class level logger
-	 */
-	private static final Logger logger = Logger.getLogger(FxViewer.class.getName());
-
-	// Attributes
-
-	/**
-	 * Name of the default view.
-	 */
-	public static String DEFAULT_VIEW_ID = "defaultView";
-
+public class FxViewer extends Viewer {
 	
-
-	/**
-	 * How does the viewer synchronise its internal graphic graph with the graph
-	 * displayed. The graph we display can be in the UI thread (as will be
-	 * the viewer, therefore in the same thread as the viewer), in another
-	 * thread, or on a distant machine.
-	 */
-	public enum ThreadingModel {
-		GRAPH_IN_GUI_THREAD, GRAPH_IN_ANOTHER_THREAD, GRAPH_ON_NETWORK
-	};
-
-	// Attribute
-
-	/**
-	 * If true the graph we display is in another thread, the synchronisation
-	 * between the graph and the graphic graph must therefore use thread
-	 * proxies.
-	 */
-	protected boolean graphInAnotherThread = true;
-
-	/**
-	 * The graph observed by the views.
-	 */
-	protected GraphicGraph graph;
-
-	/**
-	 * If we have to pump events by ourself.
-	 */
-	protected ProxyPipe pumpPipe;
-
-	/**
-	 * If we take graph events from a source in this thread.
-	 */
-	protected Source sourceInSameThread;
-
+	// Attributes
+	
 	/**
 	 * Timer in the JavaFX thread.
 	 */
@@ -161,28 +104,15 @@ public class FxViewer implements Viewer {
 	 * Delay in milliseconds between frames.
 	 */
 	protected int delay = 40;
-
+	
 	/**
-	 * The set of views.
+	 * Name of the default view.
 	 */
-	protected final Map<String, View> views = new TreeMap<String, View>();
+	public static String DEFAULT_VIEW_ID = "defaultView";
 
-	/**
-	 * What to do when a view frame is closed.
-	 */
-	protected CloseFramePolicy closeFramePolicy = CloseFramePolicy.EXIT;
-
-	// Attribute
-
-	/**
-	 * Optional layout algorithm running in another thread.
-	 */
-	protected LayoutRunner optLayout = null;
-
-	/**
-	 * If there is a layout in another thread, this is the pipe coming from it.
-	 */
-	protected ProxyPipe layoutPipeIn = null;
+	public String getDefaultID() {
+		return DEFAULT_VIEW_ID ;
+	}
 
 	// Construction
 
@@ -245,16 +175,7 @@ public class FxViewer implements Viewer {
 			throw new RuntimeException("TO DO, sorry !:-)");
 		}
 	}
-
-	/**
-	 * Create a new unique identifier for a graph.
-	 * 
-	 * @return The new identifier.
-	 */
-	public String newGGId() {
-		return String.format("GraphicGraph_%d", (int) (Math.random() * 10000));
-	}
-
+	
 	/**
 	 * Initialise the viewer.
 	 * 
@@ -267,9 +188,9 @@ public class FxViewer implements Viewer {
 	 *            The source of events from this thread (null if ppipe != null).
 	 */
 	public void init(GraphicGraph graph, ProxyPipe ppipe, Source source) {
-		this.graph = graph;
-		this.pumpPipe = ppipe;
-		this.sourceInSameThread = source;
+		super.graph = graph;
+		super.pumpPipe = ppipe;
+		super.sourceInSameThread = source;
 
 		this.timeline = new Timeline(new KeyFrame(Duration.millis(delay), actionEvent -> actionPerformed()));
 		
@@ -314,99 +235,11 @@ public class FxViewer implements Viewer {
 			timeline = null;
 		}
 	}
-
-	// Access
-	public static GraphRenderer<?, ?> newGraphRenderer() {
+	
+	@Override
+	public GraphRenderer<?, ?> newDefaultGraphRenderer() {
+		// TODO Auto-generated method stub
 		return new FxFullGraphRenderer();
-	}
-
-
-	/**
-	 * What to do when a frame is closed.
-	 */
-	public CloseFramePolicy getCloseFramePolicy() {
-		return closeFramePolicy;
-	}
-
-	/**
-	 * New proxy pipe on events coming from the viewer through a thread.
-	 * 
-	 * @return The new proxy pipe.
-	 */
-	public ProxyPipe newThreadProxyOnGraphicGraph() {
-		ThreadProxyPipe tpp = new ThreadProxyPipe();
-		tpp.init(graph);
-		return tpp;
-	}
-
-	/**
-	 * New viewer pipe on the events coming from the viewer through a thread.
-	 * 
-	 * @return The new viewer pipe.
-	 */
-	public ViewerPipe newViewerPipe() {
-		ThreadProxyPipe tpp = new ThreadProxyPipe();
-		tpp.init(graph, false);
-
-		enableXYZfeedback(true);
-
-		return new ViewerPipe(String.format("viewer_%d", (int) (Math.random() * 10000)), tpp);
-	}
-
-	/**
-	 * The underlying graphic graph. Caution : Use the returned graph only in
-	 * the UI thread !!
-	 */
-	public GraphicGraph getGraphicGraph() {
-		return graph;
-	}
-
-	/**
-	 * The view that correspond to the given identifier.
-	 * 
-	 * @param id
-	 *            The view identifier.
-	 * @return A view or null if not found.
-	 */
-	public View getView(String id) {
-		synchronized (views) {
-			return views.get(id);
-		}
-	}
-
-	/**
-	 * The default view. This is a shortcut to a call to
-	 * {@link #getView(String)} with {@link #DEFAULT_VIEW_ID} as parameter.
-	 * 
-	 * @return The default view or null if no default view has been installed.
-	 */
-	public FxViewPanel getDefaultView() {
-		return (FxViewPanel) getView(DEFAULT_VIEW_ID);
-	}
-
-	// Command
-
-	/**
-	 * Build the default graph view and insert it. The view identifier is
-	 * {@link #DEFAULT_VIEW_ID}. You can request the view to be open in its own
-	 * frame.
-	 * 
-	 * @param openInAFrame
-	 *            It true, the view is placed in a frame, else the view is only
-	 *            created and you must embed it yourself in your application.
-	 */
-	public View addDefaultView(boolean openInAFrame) {
-		synchronized (views) {
-			GraphRenderer<?, ?> renderer = newGraphRenderer();
-			View view = renderer.createDefaultView(this, DEFAULT_VIEW_ID);
-			
-			addView(view);
-			
-			if (openInAFrame)
-				view.openInAFrame(true);
-
-			return view;
-		}
 	}
 	
 	/**
@@ -421,7 +254,7 @@ public class FxViewer implements Viewer {
 	 */
 	public View addDefaultView(boolean openInAFrame, GraphRenderer<?, ?> renderer) {
 		synchronized (views) {
-			View view = renderer.createDefaultView(this, DEFAULT_VIEW_ID);
+			View view = renderer.createDefaultView(this, getDefaultID());
 			
 			addView(view);
 			
@@ -429,77 +262,6 @@ public class FxViewer implements Viewer {
 				view.openInAFrame(true);
 
 			return view;
-		}
-	}
-
-	/**
-	 * Add a view using its identifier. If there was already a view with this
-	 * identifier, it is closed and returned (if different of the one added).
-	 * 
-	 * @param view
-	 *            The view to add.
-	 * @return The old view that was at the given identifier, if any, else null.
-	 */
-	public View addView(View view) {
-		synchronized (views) {
-			View old = views.put(view.getIdView(), view);
-
-			if (old != null && old != view)
-				old.close(graph);
-
-			return old;
-		}
-	}
-
-	/**
-	 * Add a new default view with a specific renderer. If a view with the same
-	 * id exists, it is removed and closed. By default the view is open in a
-	 * frame.
-	 * 
-	 * @param id
-	 *            The new view identifier.
-	 * @param renderer
-	 *            The renderer to use.
-	 * @return The created view.
-	 */
-	public View addView(String id, GraphRenderer<?, ?> renderer) {
-		return addView(id, renderer, true);
-	}
-
-	/**
-	 * Same as {@link #addView(String, GraphRenderer)} but allows to specify
-	 * that the view uses a frame or not.
-	 * 
-	 * @param id
-	 *            The new view identifier.
-	 * @param renderer
-	 *            The renderer to use.
-	 * @param openInAFrame
-	 *            If true the view is open in a frame, else the returned view is
-	 *            a JPanel that can be inserted in a GUI.
-	 * @return The created view.
-	 */
-	public View addView(String id, GraphRenderer<?, ?> renderer, boolean openInAFrame) {
-		synchronized (views) {
-			View view = renderer.createDefaultView(this, id);
-			addView(view);
-
-			if (openInAFrame)
-				view.openInAFrame(true);
-
-			return view;
-		}
-	}
-
-	/**
-	 * Remove a view. The view is not closed.
-	 * 
-	 * @param id
-	 *            The view identifier.
-	 */
-	public void removeView(String id) {
-		synchronized (views) {
-			views.remove(id);
 		}
 	}
 
@@ -510,7 +272,6 @@ public class FxViewer implements Viewer {
 	 * automatically.
 	 */
 	public void actionPerformed() {
-
 		synchronized (views) {
 			// long t1=System.currentTimeMillis();
 			// long gsize1=graph.getNodeCount();
@@ -541,135 +302,5 @@ public class FxViewer implements Viewer {
 			// (t2-t1)/1000.0, (t3-t2)/1000.0, (t4-t3)/1000.0, (t5-t4)/1000.0,
 			// (gsize2-gsize1), gsize1, gsize2);
 		}
-	}
-
-	/**
-	 * Compute the overall bounds of the graphic graph according to the nodes
-	 * and sprites positions. We can only compute the graph bounds from the
-	 * nodes and sprites centres since the node and graph bounds may in certain
-	 * circumstances be computed according to the graph bounds. The bounds are
-	 * stored in the graph metrics.
-	 */
-	public void computeGraphMetrics() {
-		graph.computeBounds();
-
-		synchronized (views) {
-			Point3 lo = graph.getMinPos();
-			Point3 hi = graph.getMaxPos();
-			for (final View view : views.values()) {
-				Camera camera = view.getCamera();
-				if (camera != null) {
-					camera.setBounds(lo.x, lo.y, lo.z, hi.x, hi.y, hi.z);
-				}
-			}
-		}
-	}
-
-	/**
-	 * What to do when the frame containing one or more views is closed.
-	 * 
-	 * @param policy
-	 *            The close frame policy.
-	 */
-	public void setCloseFramePolicy(CloseFramePolicy policy) {
-		synchronized (views) {
-			closeFramePolicy = policy;
-		}
-	}
-
-	// Optional layout algorithm
-
-	/**
-	 * Enable or disable the "xyz" attribute change when a node is moved in the
-	 * views. By default the "xyz" attribute is changed.
-	 * 
-	 * By default, each time a node of the graphic graph is moved, its "xyz"
-	 * attribute is reset to follow the node position. This is useful only if
-	 * someone listen at the graphic graph or use the graphic graph directly.
-	 * But this operation is quite costly. Therefore by default if this viewer
-	 * runs in its own thread, and the main graph is in another thread, xyz
-	 * attribute change will be disabled until a listener is added.
-	 * 
-	 * When the viewer is created to be used only in the ui thread, this
-	 * feature is always on.
-	 */
-	public void enableXYZfeedback(boolean on) {
-		synchronized (views) {
-			graph.feedbackXYZ(on);
-		}
-	}
-
-	/**
-	 * Launch an automatic layout process that will position nodes in the
-	 * background.
-	 */
-	public void enableAutoLayout() {
-		enableAutoLayout(Layouts.newLayoutAlgorithm());
-	}
-
-	/**
-	 * Launch an automatic layout process that will position nodes in the
-	 * background.
-	 * 
-	 * @param layoutAlgorithm
-	 *            The algorithm to use (see Layouts.newLayoutAlgorithm() for the
-	 *            default algorithm).
-	 */
-	public void enableAutoLayout(Layout layoutAlgorithm) {
-		synchronized (views) {
-			if (optLayout == null) {
-				// optLayout = new LayoutRunner(graph, layoutAlgorithm, true,
-				// true);
-				optLayout = new LayoutRunner(graph, layoutAlgorithm, true, false);
-				graph.replay();
-				layoutPipeIn = optLayout.newLayoutPipe();
-				layoutPipeIn.addAttributeSink(graph);
-			}
-		}
-	}
-
-	/**
-	 * Disable the running automatic layout process, if any.
-	 */
-	public void disableAutoLayout() {
-		synchronized (views) {
-			if (optLayout != null) {
-				((ThreadProxyPipe) layoutPipeIn).unregisterFromSource();
-				layoutPipeIn.removeSink(graph);
-				layoutPipeIn = null;
-				optLayout.release();
-				optLayout = null;
-			}
-		}
-	}
-
-	/** Dirty replay of the graph. */
-	public void replayGraph(Graph graph) {
-		// Replay all graph attributes.
-
-		graph.attributeKeys().forEach(key -> {
-			graph.setAttribute(key, graph.getAttribute(key));
-		});
-
-		// Replay all nodes and their attributes.
-
-		graph.nodes().forEach(node -> {
-			Node n = this.graph.addNode(node.getId());
-
-			node.attributeKeys().forEach(key -> {
-				n.setAttribute(key, node.getAttribute(key));
-			});
-		});
-
-		// Replay all edges and their attributes.
-
-		graph.edges().forEach(edge -> {
-			Edge e = graph.addEdge(edge.getId(), edge.getSourceNode().getId(), edge.getTargetNode().getId(),
-					edge.isDirected());
-
-			edge.attributeKeys().forEach(key -> {
-				e.setAttribute(key, edge.getAttribute(key));
-			});
-		});
 	}
 }
